fe8851e4ea1a4ad9bfe4649a10eea90df5cfd1f9
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-08 20:34:21 -0300

    chore: configuring mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -21,6 +22,13 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://192.168.99.100:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
   }
 }
 
