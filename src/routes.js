9a43d9456774bda307c1a80de7c71bbd28619f29
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-07 21:45:49 -0300

    feat: add user registration

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'João Victor Vieira de Andrade',
-    email: 'joaovictorvieira.23@hotmail.com',
-    password_hash: '22091623',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
