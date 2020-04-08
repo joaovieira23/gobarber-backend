72d0afee2da03bc6f5e8c5e223ae9c155e8eadba
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-07 21:30:28 -0300

    feat: create models loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'João Victor Vieira de Andrade',
+    email: 'joaovictorvieira.23@hotmail.com',
+    password_hash: '22091623',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
