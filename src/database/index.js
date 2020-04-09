518513356deee76c23a675dd8c19932b7021d644
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-09 17:47:05 -0300

    fix: remove deprecation warnings
    add useUnifiedTopology

--- src/database/index.js
+++ src/database/index.js
@@ -28,7 +28,11 @@ class Database {
   mongo() {
     this.mongoConnection = mongoose.connect(
       'mongodb://192.168.99.100:27017/gobarber',
-      { useNewUrlParser: true, useFindAndModify: true }
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
     );
   }
 }
