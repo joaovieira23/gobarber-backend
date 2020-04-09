d180b6f1710535f9eaebadaf1934e07650a22afb
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-09 00:51:33 -0300

    chore: configure nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4d934cebbad0a9',
+    pass: '640d22485af5a7',
+  },
+  default: {
+    from: `Equipe GoBarber <noreply@gobarber.com`,
+  },
+};
