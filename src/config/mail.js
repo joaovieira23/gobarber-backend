70c6676761cf719b85bbce74bfb4b6b08b35d082
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-10 02:15:16 -0300

    feat: configure environment variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '4d934cebbad0a9',
-    pass: '640d22485af5a7',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: `Equipe GoBarber <noreply@gobarber.com`,
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,4 +1,4 @@
 export default {
-  dsn:
-    'https://0649fe24efbf461288979a2fbce2b925@o357269.ingest.sentry.io/5195783',
+  dsn: process.env.SENTRY_DSN,
+  // 'https://0649fe24efbf461288979a2fbce2b925@o357269.ingest.sentry.io/5195783',
 };
